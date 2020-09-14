a86d83d58823f5fc098f681a905c5ff7deb2fb4c
sandi <muhammadsandimaulana12@gmail.com>
2020-09-14 13:42:29 +0700

    root

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+
+app.get('/',(req,res)=>res.send('root'))
 app.listen(port,() => console.log(`express run at port: ${port}`))
